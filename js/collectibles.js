07d91e8b23ab1e178efb6501954c781cb7d0f3ba
Laludztee <oluwatomisin1605@gmail.com>
2018-09-13 14:19:38 +0100

    UPDATE: Udacity style guide

--- js/collectibles.js
+++ js/collectibles.js
@@ -10,13 +10,13 @@ let Gems = function(x, y) {
     this.y = -300;
     this.sprite = 'images/Gem Blue.png';
 	
-	this.xGemLocation = [305, 200, 130];
+    this.xGemLocation = [305, 200, 130];
     this.yGemLocation = [200, 102, 60];
 	
 	setTimeout(() => {
-			this.x = this.xGemLocation[Math.floor(Math.random() * this.xGemLocation.length)];
-			this.y = this.yGemLocation[Math.floor(Math.random() * this.yGemLocation.length)];
-		}, Math.random() * 20000);
+		this.x = this.xGemLocation[Math.floor(Math.random() * this.xGemLocation.length)];
+		this.y = this.yGemLocation[Math.floor(Math.random() * this.yGemLocation.length)];
+	}, Math.random() * 20000);
 };
 
 Gems.prototype.render = function () {
@@ -51,17 +51,17 @@ Gems.prototype.update = function () {
 
 //keys
 let Key = function(x, y) {
-	this.x = -100;
+    this.x = -100;
     this.y = -400;
     this.sprite = 'images/Key.png';
 	
-	this.xKeyLocation = [400, 300, 100];
+    this.xKeyLocation = [400, 300, 100];
     this.yKeyLocation = [230, 104, 70];
 	
 	setTimeout(() => {
-			this.x = this.xKeyLocation[Math.floor(Math.random() * this.xKeyLocation.length)];
-			this.y = this.yKeyLocation[Math.floor(Math.random() * this.yKeyLocation.length)];
-		}, Math.random() * 30000);
+		this.x = this.xKeyLocation[Math.floor(Math.random() * this.xKeyLocation.length)];
+		this.y = this.yKeyLocation[Math.floor(Math.random() * this.yKeyLocation.length)];
+	}, Math.random() * 30000);
 };
 
 Key.prototype.render = function () {
